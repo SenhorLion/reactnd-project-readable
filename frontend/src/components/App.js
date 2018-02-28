bdaf4283df011113e9379dfa0ce1e1496f66df24
Lionel Adams <adamslionel@hotmail.co.uk>
2018-02-28 17:49:25 +0000

    feat: Add Semantic UI framework

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,7 +5,50 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Readable App</h1>
+        <div class="ui fixed inverted menu">
+          <div class="ui container">
+            <a href="#" class="header item">
+              <img class="logo" src="assets/images/logo.png" />
+              Readable App
+            </a>
+            <a href="#" class="item">
+              Home
+            </a>
+            <div class="ui simple dropdown item">
+              Categories <i class="dropdown icon" />
+              <div class="menu">
+                <a class="item" href="#">
+                  Link Item
+                </a>
+                <a class="item" href="#">
+                  Link Item
+                </a>
+                <div class="divider" />
+                <div class="header">Header Item</div>
+                <div class="item">
+                  <i class="dropdown icon" />
+                  Sub Menu
+                  <div class="menu">
+                    <a class="item" href="#">
+                      Link Item
+                    </a>
+                    <a class="item" href="#">
+                      Link Item
+                    </a>
+                  </div>
+                </div>
+                <a class="item" href="#">
+                  Link Item
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="ui main text container">
+          <h1 class="ui header">Readable App</h1>
+          <p>This is a basic fixed menu template using Semantic UI.</p>
+        </div>
       </div>
     );
   }
