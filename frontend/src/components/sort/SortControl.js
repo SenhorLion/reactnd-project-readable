593826b54d2eca11d74954a9ca8c0fb7e059a1de
Lionel Adams <adamslionel@hotmail.co.uk>
2018-03-07 13:15:46 +0000

    feature: Sort control and sort filters
    - Sort control acts as a button that changes state.sortKey based on its sortKey prop
    - `sortFilter` acts as a function map that receives a list and returns the list sorted based on the sort property

---
+++ frontend/src/components/sort/SortControl.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '../button/Button';
+import classNames from 'classnames';
+
+const SortControl = ({
+  sortKey,
+  activeSortKey,
+  isSortReverse,
+  onSort,
+  children,
+}) => {
+  const isActive = sortKey === activeSortKey;
+  const sortClass = classNames('ui basic button sort-filter', {
+    active: isActive,
+  });
+  const iconClass = classNames('caret icon', {
+    up: isActive && isSortReverse,
+    down: isActive && !isSortReverse,
+  });
+  return (
+    <Button className={sortClass} onClick={() => onSort(sortKey)}>
+      <i className={iconClass} />
+      {children}{' '}
+    </Button>
+  );
+};
+
+export default SortControl;
---
+++ frontend/src/utils/sortFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { sortBy } from 'lodash';
+
+const sortFilter = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  CATEGORY: list => sortBy(list, 'category'),
+  VOTE_SCORE: list => sortBy(list, 'voteScore').reverse(), // reverse to see the highest count first
+  TIME_STAMP: list => sortBy(list, 'timeStamp').reverse(),
+};
+
+export default sortFilter;
