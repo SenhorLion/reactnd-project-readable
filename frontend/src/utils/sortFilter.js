0a7915c33d3766877b5fb872d5b2291b794a9f82
Lionel Adams <adamslionel@hotmail.co.uk>
2018-03-15 16:27:35 +0000

    bug: Fix sorting by timestamp (mis-spelt sort key!)

--- frontend/src/utils/sortFilter.js
+++ frontend/src/utils/sortFilter.js
@@ -6,7 +6,7 @@ const sortFilter = {
   TITLE: list => sortBy(list, 'title'),
   CATEGORY: list => sortBy(list, 'category'),
   VOTE_SCORE: list => sortBy(list, 'voteScore').reverse(), // reverse to see the highest count first
-  TIME_STAMP: list => sortBy(list, 'timeStamp').reverse(),
+  TIME_STAMP: list => sortBy(list, 'timestamp').reverse(),
 };
 
 export default sortFilter;
