455d74015f4cbfde2c8f5ef379bd887fc01531ce
Lionel Adams <adamslionel@hotmail.co.uk>
2018-03-16 13:26:13 +0000

    refactor: Add a category colour to posts

--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
-import { capitalize } from '../../utils/helper';
+import classNames from 'classnames';
 
-const PostItem = ({ post }) => (
-  <div className="ui yellow segment divided items post">
-    <div className="item">
-      <div className="content">
-        <a className="header">{capitalize(post.title)}</a>
-        <div className="meta">
-          <span className="author">Author: {post.author}</span>
-          <span className="date">
-            <Moment fromNow>{post.timestamp}</Moment>
-          </span>
-        </div>
-        <div className="description">
-          <p>{post.body}</p>
-        </div>
-        <div className="extra">
-          <div className="ui label">
-            <Link to={post.category}>{post.category}</Link>
+import { capitalize, CATEGORY_COLOUR_MAP } from '../../utils/helper';
+
+const PostItem = ({ post }) => {
+  const categoryColour = CATEGORY_COLOUR_MAP[post.category];
+  const postItemClass = classNames(
+    'ui  segment divided items post',
+    categoryColour
+  );
+  const labelColour = classNames('ui label', categoryColour);
+  return (
+    <div className={postItemClass}>
+      <div className="item">
+        <div className="content">
+          <a className="header">{capitalize(post.title)}</a>
+          <div className="meta">
+            <span className="author">Author: {post.author}</span>
+            <span className="date">
+              <Moment fromNow>{post.timestamp}</Moment>
+            </span>
           </div>
-          <div className="ui label">
-            <i className="like icon" /> {post.voteScore}
+          <div className="description">
+            <p>{post.body}</p>
           </div>
-          <div className="ui label">
-            <i className="comment alternate outline icon" /> {post.commentCount}
+          <div className="extra">
+            <div className={labelColour}>
+              <Link to={post.category}>{post.category}</Link>
+            </div>
+            <div className="ui label">
+              <i className="like icon" /> {post.voteScore}
+            </div>
+            <div className="ui  label">
+              <i className="comment alternate outline icon" />{' '}
+              {post.commentCount}
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default PostItem;
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,3 +1,9 @@
 export function capitalize(str = '') {
   return typeof str !== 'string' ? '' : str[0].toUpperCase() + str.slice(1);
 }
+
+export const CATEGORY_COLOUR_MAP = {
+  react: 'pink',
+  redux: 'teal',
+  udacity: 'violet',
+};
