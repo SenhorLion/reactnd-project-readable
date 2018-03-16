8d607e6dc8a63064ac085a1b22410870a6a26c5c
Lionel Adams <adamslionel@hotmail.co.uk>
2018-03-16 13:26:45 +0000

    fix: Fix app crashing when no posts are loaded

--- frontend/src/components/posts/PostsList.js
+++ frontend/src/components/posts/PostsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import cuid from 'cuid';
-
+import Loading from 'react-loading';
 import SortControl from '../sort/SortControl';
 import PostItem from './PostItem';
 
@@ -22,10 +22,15 @@ import PostItem from './PostItem';
 const PostsList = ({ list, sortKey, isSortReverse, sortFilter }) => {
   const sortedList = sortFilter[sortKey](list);
   const reverseSortedList = isSortReverse ? sortedList.reverse() : sortedList;
+  const isListLoaded = reverseSortedList.length;
 
   return (
     <div className="posts-list">
-      {reverseSortedList.map(post => <PostItem key={cuid()} post={post} />)}
+      {!isListLoaded && (
+        <Loading delay={200} type="spokes" color="#222" className="loading" />
+      )}
+      {isListLoaded &&
+        reverseSortedList.map(post => <PostItem key={cuid()} post={post} />)}
     </div>
   );
 };
