8d123db0452ba0227063a1a17576c0a909a99d75
Lionel Adams <adamslionel@hotmail.co.uk>
2018-03-27 12:42:59 +0100

    refactor: Add `type` to button component, default to `button`

--- frontend/src/components/button/Button.js
+++ frontend/src/components/button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = ({ onClick, className = '', children }) => (
-  <button onClick={onClick} className={className} type=" button">
+const Button = ({ onClick, className = '', type = 'button', children }) => (
+  <button onClick={onClick} className={className} type={type}>
     {' '}
     {children}{' '}
   </button>
