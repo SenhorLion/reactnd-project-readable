ebe0c440d395b9385ed419a8c42d4a9f38438056
Lionel Adams <adamslionel@hotmail.co.uk>
2018-04-10 16:10:05 +0100

    refactor: Simplify returning post by id
    - Utilise the data structure to return post by Id: e.g posts.items[postId]

--- frontend/src/containers/Post.js
+++ frontend/src/containers/Post.js
@@ -4,22 +4,13 @@ import { withRouter } from 'react-router-dom';
 import { onDeletePost, fetchAllPosts } from '../actions';
 import PostDetailView from '../components/posts/PostDetailView';
 
-const getPostToShow = (posts, postId) => {
-  if (posts && postId) {
-    return Object.values(posts).find(post => post.id === postId);
-  }
-
-  return {};
-};
-
-// TODO: dispatch action to fetch post
 const mapStateToProps = ({ posts }, ownProps) => {
   const { postId } = ownProps;
 
   return {
     post: {
       isFetching: posts.isFetching,
-      item: getPostToShow(posts.items, postId),
+      item: posts.items[postId],
     },
   };
 };
